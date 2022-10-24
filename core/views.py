5fda5538510dceeef67d4831968214c39ec47ff1
Aniket Tyagi <t.aniket561@gmail.com>
2022-10-24 12:34:30 +0530

    Filter feature added
    Now students can only see posts from their class and teachers can filter through classes to choose a class they want to see posts from

--- core/views.py
+++ core/views.py
@@ -5,20 +5,24 @@ from .models import Post
 from accounts.models import User
 from .forms import NewPostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import Group
+from django.db.models import Q
 
 
 def home_view(request):
   def user_in_group(user, group):
     return user.groups.all().filter(name=group).exists()
 
-  if user_in_group(request.user, "Students"):
-    posts = Post.objects.filter()
+  if not user_in_group(request.user, "Teachers"):
+    posts = Post.objects.filter(posted_by__grade=request.user.grade) | Post.objects.filter(for_grade=request.user.grade)
 
   else:
-    posts = Post.objects.all()
+    if request.GET.get('class'):
+      class_filter = request.GET.get('class')
+      posts = Post.objects.filter(posted_by__grade=class_filter) | Post.objects.filter(for_grade=class_filter)
+    else:
+      posts = Post.objects.all()
 
-  context = {'posts': posts}
+  context = {'posts': posts, 'range': range(1, 13)}
 
   return render(request, 'core/home.html', context)
 
